3af74495738da225fd6f01e5181eeea6d240a1b0
Wes <wesleyayer92@gmail.com>
2020-05-18 10:51:35 -0400

    basic templating

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const express = require(express);
+const express = require('express');
 const app = express();
 const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Hello',
+        special: 'hamburgers'
+    });
+});
 
 app.listen(PORT);
